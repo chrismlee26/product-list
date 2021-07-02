45ea39ab8cef49d73d66f2a343a307590bc397f1
CHRIS_LEE <chrismlee87@gmail.com>
2021-07-02 13:34:21 -0700

    reducer for cart working

--- src/reducers/ShoppingCartReducer.js
+++ src/reducers/ShoppingCartReducer.js
@@ -1,16 +1,24 @@
 import { ADD_TO_CART } from '../actions'
 
 const shoppingCartReducer = (state = [], action) => {
+  console.log(action)
+  console.log(state)
   switch(action.type) {
     case ADD_TO_CART:
       // loop over state
+      for (let i = 0; i < state.length; i+= 1) {
         // if item id === index (payload)
+        if (state[i].id === action.payload.index) {
           // item qty + 1
+          state[i].qty += 1
           // return new state
-      
+          return [...state]
+        }
+
+      }
       //  add new {id: index (payload), qty: 1}
 
-      return [...state, action.payload.index]
+      return [...state, { id: action.payload.index, qty: 1 }]
     default:
       return state
   }
